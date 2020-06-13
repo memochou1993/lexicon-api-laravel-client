ef91fe9b2473194b21f6f13a4da95ee60510adcc
Memo Chou <memochou1993@gmail.com>
2020-06-13 23:40:40 +0800

    Fix tests

--- tests/Http/EventControllerTest.php
+++ tests/Http/EventControllerTest.php
@@ -4,6 +4,7 @@ namespace MemoChou1993\Localize\Tests\Http;
 
 use MemoChou1993\Localize\Facades\Localize;
 use MemoChou1993\Localize\Tests\TestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class EventControllerTest extends TestCase
 {
@@ -20,7 +21,7 @@ class EventControllerTest extends TestCase
                     'sync',
                 ],
             ])
-            ->assertNoContent();
+            ->assertStatus(Response::HTTP_ACCEPTED);
 
         $language = Localize::getLanguages()->first();
 
